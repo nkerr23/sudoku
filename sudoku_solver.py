aeb7410abede86ddc3eabedbbc9fd83f8809004f
= <ninarkerr@gmail.com>
2024-12-22 16:58:31 -0800

    added functions to check for val in rows, cols and squares

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,8 +3,31 @@
 
 import sys
 
+# return true if item doesn't exist in row, false otherwise
+def check_row (board, row, value):
+    for item in board[row]:
+        if item == value:
+            return False
+    return True
+
+# return true if item doesn't exist in col, false otherwise  
+def check_column (board, col, value):
+    for row in board:
+        if row[col] == value:
+            return False
+    return True
+
+# return true if item doesn't exist in 3x3 square, false otherwise
+def check_square (board, row, col, value):
+    for i in range((row%3)*3, 3):
+        for j in range((col%3)*3, 3):
+            if board[i][j] == value:
+                return False
+    return True
+
 # take in 9 by 9 array
 def solve_array (board):
+    # create function that adds checks what it can add to the first corner
     return 0
 
 def main():
@@ -32,5 +55,6 @@ if __name__ == "__main__":
              [0, 6, 0, 0, 0, 0, 0, 0, 1],
              [0, 0, 0, 0, 5, 0, 6, 0, 4],
              [0, 0, 0, 2, 8, 0, 0, 3, 0]]
-
+    solve_array(input)
+    
     # main
