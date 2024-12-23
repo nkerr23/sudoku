c87a0a8926366f4a75c51709d3c65cd70032f6cd
= <ninarkerr@gmail.com>
2024-12-22 19:37:46 -0800

    runs, but error in too any recursion attepts

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -27,23 +27,48 @@ def check_square (board, row, col, value):
 
 # return true if can place the num in that square, false otherwise
 def can_place_val(board, row, col, value):
-    if check_row[board, row, value]:
-        if check_column[board, col, value]:
-            if check_square[board, row, col, value]:
+    if check_row(board, row, value):
+        if check_column(board, col, value):
+            if check_square(board, row, col, value):
                 return True
     return False
 
+# return the next empty square, if non existent, return None
+def find_empty_square(board):
+    for r, row in enumerate(board):
+        for c, item in enumerate(row):
+            if item == 0:
+                return [r, c]
+    return [None, None]
+
+# try and fill the spot, if can't, return None
+def fill_spot(board, row, col):
+    for val in range(1,9):
+        if can_place_val(board, row, col, val):
+            board[row][col] = val
+            return board
+    return [[None]]
+
 # take in 9 by 9 array
-def solve_array (board):
-    cur_val = 1
-    for row in board:
-        for col in board:
-            if board[row][col] == 0:
-                continue
-            else:
-                if (check_row[cur_val] and check_column[cur_val] and check_square[cur_val])
-    # create function that adds checks what it can add to the first corner
-    return 0
+def solve_array(board, filled_squares): # filled squares is a list of filled squares
+    cur_board = board
+    cur = filled_squares[-1]
+    print(cur)
+    
+    # check if solved
+    if cur[0] == None:
+        return cur_board
+    
+    # try and put a value in the next empty square, if works, return new board
+    new_board = fill_spot(cur_board, cur[0], cur[1])
+    if new_board[0][0] != None:
+        new = find_empty_square(cur_board)
+        filled_squares.append(new)
+        return solve_array(new_board, filled_squares)
+    
+    # if no value could fit in the current square, try prev square again with new val
+    filled_squares.pop()
+    return solve_array(cur_board, filled_squares)
 
 def main():
     # ensure 1 argument
@@ -61,15 +86,21 @@ def main():
     solve_array(sys.argv[1])
 
 if __name__ == "__main__":
-    input = [[0, 0, 0, 0, 1, 0, 0, 0, 0],
-             [0, 0, 8, 3, 0, 0, 0, 0, 0],
-             [0, 0, 9, 0, 0, 2, 0, 1, 3],
-             [2, 0, 0, 0, 4, 0, 7, 0, 0],
-             [0, 4, 0, 6, 3, 0, 0, 5, 0],
-             [9, 0, 0, 8, 0, 0, 0, 0, 0],
-             [0, 6, 0, 0, 0, 0, 0, 0, 1],
-             [0, 0, 0, 0, 5, 0, 6, 0, 4],
-             [0, 0, 0, 2, 8, 0, 0, 3, 0]]
-    solve_array(input)
+    input = [
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 8, 3, 0, 0, 0, 0, 0],
+            [0, 0, 9, 0, 0, 2, 0, 1, 3],
+            [2, 0, 0, 0, 4, 0, 7, 0, 0],
+            [0, 4, 0, 6, 3, 0, 0, 5, 0],
+            [9, 0, 0, 8, 0, 0, 0, 0, 0],
+            [0, 6, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 5, 0, 6, 0, 4],
+            [0, 0, 0, 2, 8, 0, 0, 3, 0]]
+    
+    #print(can_place_val(input, 0, 0, 4))
+    first = find_empty_square(input)
+    filled_squares = [first]
+    final = (solve_array(input, filled_squares))
+    print(final)
     
     # main
